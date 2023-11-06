1f75b8bd8c6fe7441a04e8340d0041bb9b541567
AAl-Awdi <127621473+AAl-Awdi@users.noreply.github.com>
2023-11-06 16:34:39 +0000

    Add files via upload
    I created 3 Classes (All generic, so any data types can be used):
    
    Node - To create, store, and retrieve information on the nodes in the Binary Tree and Binary Search Tree. 
    
    BinTree - To create the binary tree "shape", and some sorting algorithms and a searching algorithm.
    
    BSTree - To create the binary search tree "shape". Child class of the "BinTree" class, so it inherits its sorting algorithms. However, it has its own searching algorithm, due to its unique nature.

---
+++ BinaryTree/BinaryTree/BSTree.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class BSTree<T> : BinTree<T> where T : IComparable
+    {  //root declared as protected in Parent Class – Binary Tree
+        public BSTree()
+        {
+            root = null;
+        }
+
+        public void InsertItem(T item)
+        {
+            insertItem(item, ref root);
+        }
+
+        private void insertItem(T item, ref Node<T> tree)
+        {
+            if (tree == null)
+                tree = new Node<T>(item);
+
+            else if (item.CompareTo(tree.Data) < 0)
+                insertItem(item, ref tree.Left);
+
+            else if (item.CompareTo(tree.Data) > 0)
+                insertItem(item, ref tree.Right);
+        }
+
+        public int Height()
+        //Return the max level of the tree
+        {
+            return Hheight(root); 
+        }
+
+        private int Hheight(Node<T> tree)
+        //Return the height of a subtree rooted at tree
+        {
+            if (tree == null) //base case: empty tree has height 0
+                return 0;
+            else //recursive case: height is 1 + max of left and right subtree heights
+                return 1 + Math.Max(Hheight(tree.Left), Hheight(tree.Right));
+        }
+
+        public int Count()
+        //Return the number of nodes in the tree
+        {
+            return Ccount(root);
+        }
+
+        private int Ccount(Node<T> tree)
+        //Return the number of nodes in the tree
+        {
+            if (tree == null) //base case: node = null
+                return 0;
+            else //recursive case: count is 1 + left and right nodes
+                return 1 + Ccount(tree.Left) + Ccount(tree.Right);
+        }
+
+        public Boolean Contains(T item)
+        //Return true if the item is contained in the BSTree
+        {
+            return Ccontains(item, root);
+        }
+
+        private Boolean Ccontains(T item, Node<T> tree)
+        //Return true if the item is contained in a subtree rooted at tree, false otherwise.
+        {
+            if (tree == null) //base case: empty tree does not contain the item
+                return false;
+            else if (item.CompareTo(tree.Data) == 0) //base case: item matches the current node's value
+                return true;
+            else if (item.CompareTo(tree.Data) < 0) //recursive case: item is smaller than the current node's value, search in the left subtree
+                return Ccontains(item, tree.Left);
+            else //recursive case: item is larger than the current node's value, search in the right subtree
+                return Ccontains(item, tree.Right);
+        }
+
+
+
+    }
+}
---
+++ BinaryTree/BinaryTree/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string buffer = "";
+            //right=r
+            //left=l
+            /////////////////////// BinTree ///////////////////////
+            Node<int> rootnode = new Node<int>(1);
+            Node<int> lchild = new Node<int>(2);
+            Node<int> rchild = new Node<int>(3);
+            Node<int> lchild_l = new Node<int>(4);
+            Node<int> lchild_r = new Node<int>(5);
+            Node<int> rchild_l = new Node<int>(6);
+            Node<int> rchild_r = new Node<int>(7);
+            Node<int> lchild_r_r = new Node<int>(8);
+
+            rootnode.Left = lchild; //2
+            rootnode.Right = rchild; //3
+            rootnode.Left.Left = lchild_l; //4
+            rootnode.Left.Right = lchild_r; //5
+            rootnode.Right.Left = rchild_l; //6
+            rootnode.Right.Right = rchild_r; //7
+            rootnode.Left.Right.Right = lchild_r_r; //8
+
+            //creates a BinTree with root = rootnode
+            BinTree<int> mytree = new BinTree<int>(rootnode);
+
+            //InOrder
+            buffer = "";
+            mytree.InOrder(ref buffer); ;
+            //displays the content of the tree
+            Console.WriteLine("in-order visit of BinTree " + buffer);
+            Console.ReadLine();
+            //PreOrder
+            buffer = "";
+            mytree.PreOrder(ref buffer); ;
+            //displays the content of the tree
+            Console.WriteLine("pre-order visit of BinTree " + buffer);
+            Console.ReadLine();
+            //PostOrder
+            buffer = "";
+            mytree.PostOrder(ref buffer); ;
+            //displays the content of the tree
+            Console.WriteLine("post-order visit of BinTree " + buffer);
+            Console.ReadLine();
+
+
+            /////////////////////// BSTree //////////////////////s
+
+            //creates a BinTree that only contains strings
+            BSTree<string> bstree = new BSTree<string>();
+
+            //inserting/creating the strings/nodes
+            bstree.InsertItem("e");
+            bstree.InsertItem("b");
+            bstree.InsertItem("g");
+            bstree.InsertItem("a");
+            bstree.InsertItem("c");
+            bstree.InsertItem("f");
+            bstree.InsertItem("h");
+            bstree.InsertItem("d");
+
+            //InOrder
+            buffer = "";
+            bstree.InOrder(ref buffer); ;
+            //displays the content of the tree
+            Console.WriteLine("in-order visit of BinSearchTree " + buffer);
+            Console.ReadLine();
+            //PreOrder
+            buffer = "";
+            bstree.PreOrder(ref buffer); ;
+            //displays the content of the tree
+            Console.WriteLine("pre-order visit of BinSearchTree " + buffer);
+            Console.ReadLine();
+            //PostOrder
+            buffer = "";
+            bstree.PostOrder(ref buffer); ;
+            //displays the content of the tree
+            Console.WriteLine("post-order visit of BinSearchTree " + buffer);
+            Console.ReadLine();
+
+
+            //displays the height of the tree
+            Console.WriteLine("Height of BinSearchTree = {0} ", bstree.Height());
+            Console.ReadLine();
+
+            //displays the number of nodes in the tree
+            Console.WriteLine("Number of nodes in the BinSearchTree = {0} ", bstree.Count());
+            Console.ReadLine();
+
+            //displays whether or not the tree contains "e"
+            Console.WriteLine("Is 'e' BinSearchTree = {0} ", bstree.Contains("e"));
+            Console.ReadLine();
+
+            //displays whether or not the tree contains "z"
+            Console.WriteLine("Is 'z' BinSearchTree = {0} ", bstree.Contains("z"));
+            Console.ReadLine();
+
+            //displays whether or not the tree contains "5"
+            Console.WriteLine("Is '5' BinSearchTree = {0} ", bstree.Contains("5"));
+            Console.ReadLine();
+        }
+    }
+}
